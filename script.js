c1dcc3fbf8bed5b2dbfe167fcbf96e03775f9b2a
Sonu Kumar Singh <hi.ersonusingh@gmail.com>
2023-04-22 16:49:30 +0530

    .

---
+++ script.js
@@ -0,0 +1,131 @@
+
+// functional : 
+
+// Object Oriented : 
+
+// Object Oriented Programming : 
+// paradigm based on objects: 
+
+// features : 
+
+// data : properties
+// methods : 
+
+// Class : 
+
+// User{
+//     name
+//     age
+//     salary
+// }
+
+// Phone{
+//     home(){}
+//     play
+//     rearcamera
+//     frontcamera
+// }
+
+
+// four pillers of oops : 
+
+// abstraction : 
+
+// Encapsulation : 
+
+// Inheritance : 
+
+// Polymorphism : 
+// add(name)
+// add(name, age)
+// add(name, age, salary)
+
+
+// class Animal{
+//     display()
+//     {
+//         console.log('This is animal class');
+//     }
+// }
+
+// const obj = new Animal();
+// obj.display();
+
+
+// class Animal{
+//     detailsAnimal(name, age)
+//     {
+//         console.log(`Name of Animal is : ${name} and age is ${age}`);
+//     }
+// }
+
+// const obj = new Animal();
+
+// obj.detailsAnimal("Tommy",5);
+
+
+// class Animal{
+//     constructor(name, age)
+//     {
+//         console.log(`Name of Animal is ${name} and age is ${age} years`);
+//     }
+// }
+
+// const obj = new Animal("Ricky",6);
+
+// Constructor : default constructor : 
+
+// class Shape{
+//     constructor()
+//     {
+//         console.log("This is default constructor");
+//     }
+// }
+
+// const obj = new Shape();
+
+
+// class Shape{
+//     constructor(l,b)
+//     {
+//         let length = l;
+//         let breadth =b;
+//         let area = length * breadth;
+//         console.log(`Area of rectangle is ${area}`);
+
+//     }
+// }
+
+// const obj = new Shape(25,25);
+
+
+// class Shape{
+//     constructor(l, b, h)
+//     {
+//         this.l = l;
+//         this.b = b;
+//         this.h = h;
+//         let vol  = l * b * h;
+//         console.log(`Volume of cuboid is : ${vol}`);
+//     }
+// }
+
+// const obj = new Shape(1,2,3);
+
+
+// class Employee{
+//     constructor(name, age, salary)
+//     {
+//         this.name = name;
+//         this.age = age;
+//         this.salary = salary;
+//     }
+//     displayDetails()
+//     {
+//         console.log(`Details of employee is: `);
+//         console.log(`Welcome: ${this.name} your age is ${this.age} years will get salary ${this.salary} `);
+//     }
+// }
+
+// const obj = new Employee("Divyesh",25,18000);
+// obj.displayDetails();
